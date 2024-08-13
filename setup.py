196f904b096a6f3cfe3cda0c6b35b7802e0230c5
Gabo <gabriel.piles.glez@gmail.com>
2024-08-13 12:04:45 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=PROJECT_NAME,
     packages=["ml_cloud_connector"],
     package_dir={"": "src"},
-    version="0.4",
+    version="0.5",
     url="https://github.com/gabriel-piles/ml-cloud-connector",
     author="HURIDOCS",
     description="This tool is a ml cloud connector",
