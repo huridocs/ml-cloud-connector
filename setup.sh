b6b562ebd8517a8bdd0cc9675ecd2a02b164b897
Ali Altıparmak <ali@huridocs.org>
2024-08-14 17:21:29 +0300

    Update setup sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt install python3.10-venv -y
 apt install python3.11-venv -y
 apt install python3.12-venv -y
 pip install --upgrade pip
-gcloud auth application-default login
+gcloud auth login
 gcloud compute firewall-rules create allow-ollama --allow tcp:11434 --target-tags=ollama-server
 INSTANCE_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname -H Metadata-Flavor:Google | cut -d . -f1)
 ZONE=$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google" | awk -F'/' '{print $NF}')
