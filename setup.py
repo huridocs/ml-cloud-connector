fe4cf97a2ccf6d531ef87fa1d0a2b2b8c8ecc5be
Ali Altıparmak <ali@huridocs.org>
2024-08-14 16:51:13 +0300

    Update setup sh

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=PROJECT_NAME,
     packages=["ml_cloud_connector"],
     package_dir={"": "src"},
-    version="0.5",
+    version="0.55",
     url="https://github.com/gabriel-piles/ml-cloud-connector",
     author="HURIDOCS",
     description="This tool is a ml cloud connector",
