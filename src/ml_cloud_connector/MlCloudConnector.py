cfaf1e509fe4fae6448c257447992687deccac3a
Ali Altıparmak <ali@huridocs.org>
2024-09-24 17:26:30 +0300

    Update connector

--- src/ml_cloud_connector/MlCloudConnector.py
+++ src/ml_cloud_connector/MlCloudConnector.py
@@ -3,13 +3,9 @@ import logging
 import tempfile
 import time
 import inspect
-from datetime import datetime
 from os import remove
-
 from requests.exceptions import ConnectionError
-from google.api_core.exceptions import GoogleAPICallError
 from googleapiclient import discovery
-from googleapiclient.errors import HttpError
 from httpx import ConnectTimeout, HTTPStatusError, ReadTimeout, RemoteProtocolError, ConnectError
 from pathlib import Path
 from typing import Callable
@@ -47,7 +43,9 @@ class MlCloudConnector:
             self.instance = cache_content["INSTANCE"]
         else:
             self.service_logger.info("No cache found. Creating new instance.")
-            self.create_initial_instance()
+            switched = False
+            while not switched:
+                switched = self.switch_to_new_instance()
 
     def is_active(self):
         instance_info = self.client.get(project=self.project, zone=self.zone, instance=self.instance)
@@ -101,6 +99,10 @@ class MlCloudConnector:
             return False
         return self.start()
 
+    def is_gpu_available(self):
+        instance_info = self.client.get(project=self.project, zone=self.zone, instance=self.instance)
+        return True if instance_info.guest_accelerators else False
+
     def get_ip(self):
         self.start_attempt_with_instance_switch()
         cache_content_dict = json.loads(self.CLOUD_CACHE_PATH.read_text())
@@ -144,97 +146,42 @@ class MlCloudConnector:
                 raise Exception(f"Error in executing the function: {str(e)}")
         return None, False, "Response not returned. Server error."
 
-    def is_gpu_available(self):
-        instance_info = self.client.get(project=self.project, zone=self.zone, instance=self.instance)
-        return True if instance_info.guest_accelerators else False
-
-    def get_zones_with_accelerator(self, compute, accelerator_type="nvidia-l4", machine_type="g2-standard-4"):
-        zones_with_accelerator = []
-        zones_request = compute.zones().list(project=self.project)
-        self.service_logger.info(f"\nGetting available zones for '{accelerator_type}' and '{machine_type}'...")
-
-        while zones_request is not None:
-            response = zones_request.execute()
-            for zone in response.get("items", []):
-                zone_name = zone["name"]
-                try:
-                    zone_available = self.is_zone_available(accelerator_type, compute, machine_type, zone_name)
-                    if zone_available:
-                        zones_with_accelerator.append(zone_name)
-                except HttpError as e:
-                    self.service_logger.info(f"Error checking accelerators in zone {zone_name}: {e}")
-            zones_request = compute.zones().list_next(previous_request=zones_request, previous_response=response)
-
-        self.service_logger.info(f"Available zones for '{accelerator_type}' and '{machine_type}': {zones_with_accelerator}")
-
-        return zones_with_accelerator
-
-    def is_zone_available(self, accelerator_type, compute, machine_type, zone_name):
-        accelerator_types = compute.acceleratorTypes().list(project=self.project, zone=zone_name).execute()
-        has_accelerator = any(acc["name"] == accelerator_type for acc in accelerator_types.get("items", []))
-        machine_types = compute.machineTypes().list(project=self.project, zone=zone_name).execute()
-        has_machine_type = any(mt["name"] == machine_type for mt in machine_types.get("items", []))
-        return has_accelerator and has_machine_type
-
-
-    def create_instance_from_snapshot(self, compute, snapshot_name):
-        instance_operator = MlCloudInstanceOperator(self.project, self.service_logger)
-        disk_operator = MlCloudDiskOperator(self.project, self.service_logger)
-        available_zones = self.get_zones_with_accelerator(compute)
-        target_zones = [zone for zone in available_zones if zone.startswith("europe-west4")]
-
-        for target_zone in target_zones:
-            current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-            new_disk_name = f"{self.server_type}-server-disk-" + current_time
-            new_instance_name = f"{self.server_type}-server-instance-" + current_time
-            self.service_logger.info(f"\nAttempting to create instance in zone: {target_zone}")
-            disk_operator.prepare_disk(compute, target_zone, new_disk_name, snapshot_name)
-            try:
-                new_instance = instance_operator.create_instance(compute, target_zone, new_disk_name, new_instance_name)
-                self.service_logger.info(f"Instance created in zone {target_zone}.")
-                self.instance = new_instance["id"]
-                self.zone = target_zone
-                self.CLOUD_CACHE_PATH.write_text(json.dumps({"ZONE": self.zone, "INSTANCE": self.instance}))
-                return True
-
-            except (GoogleAPICallError, HttpError) as err:
-                self.service_logger.info(
-                    f"An error occurred while creating the instance on {target_zone}: {err}"
-                )
-                disk_operator.delete_disk(target_zone, new_disk_name)
-                continue
-
-            except Exception as e:
-                self.service_logger.info(f"An unexpected error occurred: {e}")
-                raise
-        return False
-
-
-    def create_initial_instance(self):
-        compute = discovery.build("compute", "v1")
-        snapshot_name = f"{self.server_type}-server-snapshot"
-        return self.create_instance_from_snapshot(compute, snapshot_name)
-
+    def set_new_instance_features(self, instance_id, zone):
+        self.instance = instance_id
+        self.zone = zone
+        self.CLOUD_CACHE_PATH.write_text(json.dumps({"INSTANCE": instance_id, "ZONE": zone}))
 
     def switch_to_new_instance(self):
         compute = discovery.build("compute", "v1")
-        snapshot_name = f"{self.server_type}-server-snapshot"
+        instance_operator = MlCloudInstanceOperator(self.project, self.service_logger, self.server_type)
+        instance_id, zone = instance_operator.create_instance_from_snapshot(compute)
+        if not instance_id:
+            return False
+        self.set_new_instance_features(instance_id, zone)
+        return True
+
+    def switch_to_new_instance_with_base_instance(self):
+        compute = discovery.build("compute", "v1")
         disk_operator = MlCloudDiskOperator(self.project, self.service_logger)
-        snapshot_operator = MlCloudSnapshotOperator(self.project, self.service_logger)
-        instance_operator = MlCloudInstanceOperator(self.project, self.service_logger)
+        snapshot_operator = MlCloudSnapshotOperator(self.project, self.service_logger, self.server_type)
+        instance_operator = MlCloudInstanceOperator(self.project, self.service_logger, self.server_type)
         self.stop()
         base_instance = instance_operator.get_instance_configuration(compute, self.project, self.zone, self.instance)
         boot_disk = disk_operator.get_boot_disk(base_instance)
-        snapshot_operator.prepare_snapshot(compute, self.zone, snapshot_name, boot_disk)
-
-        return self.create_instance_from_snapshot(compute, snapshot_name)
+        snapshot_operator.prepare_snapshot(compute, self.zone, boot_disk)
+        instance_id, zone = instance_operator.create_instance_from_snapshot(compute)
+        if not instance_id:
+            return False
+        self.set_new_instance_features(instance_id, zone)
+        return True
 
     @staticmethod
     def delete_cache():
-        remove(MlCloudConnector.CLOUD_CACHE_PATH)
+        if MlCloudConnector.CLOUD_CACHE_PATH.exists():
+            remove(MlCloudConnector.CLOUD_CACHE_PATH)
 
 
 if __name__ == "__main__":
-    connector = MlCloudConnector("translation")
-    connector.switch_to_new_instance()
-
+    # connector = MlCloudConnector("translation")
+    # connector.switch_to_new_instance()
+    MlCloudConnector.delete_cache()
