5f6ed5c41b0412dd35cf1fd136e2874602e943ac
Gabo <gabriel.piles.glez@gmail.com>
2025-08-11 11:50:36 +0200

    Delete from cloud storage

--- src/ml_cloud_connector/adapters/google_v2/GoogleCloudStorage.py
+++ src/ml_cloud_connector/adapters/google_v2/GoogleCloudStorage.py
@@ -65,9 +65,21 @@ class GoogleCloudStorage(StorageProviderRepository):
 
         return True
 
+    def delete_from_cloud(self, parent_folder_name: str, folder_name: str) -> bool:
+        bucket = self.client.bucket(self.bucket_name)
+        folder = Path(parent_folder_name, folder_name)
+        blobs = bucket.list_blobs(prefix=str(folder))
+
+        for blob in blobs:
+            blob.delete()
+            print(f"Deleted {blob.name} from {self.bucket_name}")
+
+        return True
+
 
 if __name__ == "__main__":
     server_parameters = ServerParameters(namespace="google_v2", server_type=ServerType.METADATA_EXTRACTION)
     gcs = GoogleCloudStorage(server_parameters, logging.getLogger())
-    gcs.upload_to_cloud("tenant_1", Path("/"))
-    gcs.copy_from_cloud(Path("tenant_1", "extraction_id_2"), Path("/"))
+    gcs.upload_to_cloud("tenant_1", Path())
+    gcs.copy_from_cloud(Path("tenant_1", "extraction_id_2"), Path())
+    gcs.delete_from_cloud("tenant_1", "extraction_id")
